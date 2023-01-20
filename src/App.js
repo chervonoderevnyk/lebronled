591ce2f9a50a808c065d689d541d35a74ff7ca72
bigbird <igorchornyj@gmail.com>
2023-01-20 09:13:47 +0200

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 const App = () => {
     return (
         <div>
-            App
+            <div>
+                <div>name Homer</div>
+                <div>surname Simpson</div>
+                <img src="https://upload.wikimedia.org/wikipedia/uk/0/02/Homer_Simpson_2006.png" alt="homer"/>
+            </div>
+            <div>
+                <div>name Marge</div>
+                <div>surname Simpson</div>
+                <img src="https://upload.wikimedia.org/wikipedia/uk/0/0b/Marge_Simpson.png" alt="marge"/>
+            </div>
         </div>
     );
 };
