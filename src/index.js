8b27bec53ec1906a697016dba37192ece83b023e
bigbird <igorchornyj@gmail.com>
2023-02-17 11:06:11 +0200

    lesson6HomeWorkCars

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ import {setupStore} from "./redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  const store =setupStore();
+const store =setupStore();
 
 root.render(
     <Provider store={store}>
-    <App/>
+        <App/>
     </Provider>
 );
-
 
