15fbc248c9da84207a6826b7e9488ed823edd4ab
bigbird <igorchornyj@gmail.com>
2023-02-18 15:01:59 +0200

    lesson8TypescriptHomeWork

--- app.ts
+++ app.ts
@@ -1,100 +1,70 @@
-// type MyType = number | string[] | boolean
-// const test = (name: string): MyType => {
-//     return [name]
-// }
-//
-// console.log((test('Max')));
-//
-// // const arr:number[] =[1,2,3,4]
-// const arr: [number, string] = [2, '3']
+///////1////////////
+interface ICores {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string
+    }
+}
 
-// interface IUser<Data> {
-//     name: string;
-//     surname: string;
-//     age: number;
-//     status: boolean;
-//     data:Data;
-// }
-//
-// // const user: IUser = {name:'Max', surname:'Popov', age:18, status:true}
-// // const user: Partial<IUser> = {name:'Max', surname:'Popov'}
-// const user: IUser<number[]>={name:'Max', surname:'Popov', age:18, status:true, data:[1,2,3,4]}
+interface IPayload {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
 
-// class User {
-//     // private name: string
-//     // private age: number
-//     // constructor(name:string, age:number) {
-//     //     this.name=name;
-//     //     this.age=age;
-// // }
-//     constructor(private name: string, private age: number) {
-//     }
-// }
-
-// interface IShape {
-//     area: () => number;
-//     perimeter: () => number;
-// }
-//
-// interface IGreeting {
-//     greeting: () => void;
-// }
-//
-// class Rectangle implements IGreeting, IShape {
-//     constructor(private a: number, private b: number) {
-//     }
-//
-//     greeting(): void {
-//         console.log('hello');
-//     }
-//
-//     area(): number {
-//         return this.a * this.b;
-//     }
-//
-//     perimeter(): number {
-//         return this.a * 2 + this.b * 2;
-//     }
-// }
-//
-// const rectangle = new Rectangle(1, 2);
-// rectangle.greeting()
-// console.log(rectangle.area());
-// console.log(rectangle.perimeter());
-//
-// class Triangle implements IShape {
-//     constructor(private a: number, private b: number, private c: number) {
-//     }
-//
-//     area(): number {
-//         return this.a * this.b * this.c;
-//     }
-//
-//     perimeter(): number {
-//         return this.a + this.b + this.c;
-//     }
-// }
-//
-//
-// const shapes:IShape[]=[new Rectangle(1,2), new  Triangle(1,2,3), new Rectangle(4,5)]
-//
-// for (let shape of shapes){
-//     console.log(shape.area());
-//     console.log(shape.perimeter());
-// }
+interface IStarLink {
+    mission_name: string,
+    launch_date_local: string,
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: string,
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: ICores[]
+        },
+        second_stage: {
+            payloads: IPayload[]
+        }
+    }
+}
 
 
-// const x = (): number=>{
-//     return 15
-// }
+//////2////////////////
 
-// type MuType = ReturnType<typeof x>
-//
-// const a:MuType= 'sss'
+interface IUser {
+    name: string,
+    age: number,
+    gender: string,
+}
+
+const user: IUser = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+}
+
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc
+    return someUser
+}
+
+console.log(sum(1, 2));
+showSum(2, 3)
+incAge(user, 2)
 
 
 
-import {userService} from "./services/userService";
-userService.getAll().then(({data})=>{
-    console.log(data[0].address.geo.lng);
-})
